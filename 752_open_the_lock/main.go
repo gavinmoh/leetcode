60c30c7a45c8b2939b798236f862400a3d8f3b32
Gavin Moh <8167652+gavinmoh@users.noreply.github.com>
2024-04-22 11:18:22 +0800

    Updated 752 Open The Lock

--- 752_open_the_lock/main.go
+++ 752_open_the_lock/main.go
@@ -1,6 +1,10 @@
 package main
 
 func openLock(deadends []string, target string) int {
+	if target == "0000" {
+		return 0
+	}
+
 	deadendsHash := make(map[string]bool)
 	for _, deadend := range deadends {
 		deadendsHash[deadend] = true
@@ -13,13 +17,9 @@ func openLock(deadends []string, target string) int {
 	visited := make(map[string]bool)
 
 	queue := []string{"0000"}
-	for i := 0; len(queue) > 0; i++ {
+	for i := 1; len(queue) > 0; i++ {
 		newQueue := []string{}
 		for _, combo := range queue {
-			if combo == target {
-				return i
-			}
-
 			if visited[combo] || deadendsHash[combo] {
 				continue
 			}
@@ -28,37 +28,28 @@ func openLock(deadends []string, target string) int {
 			visited[combo] = true
 
 			// looping through all the position
-			for j := 0; j < 4; j++ {
-				// increment
-				newQueue = append(newQueue, increment(combo, j))
-				// descrement
-				newQueue = append(newQueue, decrement(combo, j))
+			for _, pos := range []int{0, 1, 2, 3} {
+				for _, val := range []int{1, -1} {
+					newCombo := []rune(combo)
+					num := newCombo[pos] + rune(val)
+					if num > '9' {
+						num = '0'
+					} else if num < '0' {
+						num = '9'
+					}
+					newCombo[pos] = num
+					newComboStr := string(newCombo)
+
+					if newComboStr == target {
+						return i
+					}
+
+					newQueue = append(newQueue, newComboStr)
+				}
 			}
 		}
 		queue = newQueue
 	}
 
 	return -1
-}
-
-func increment(combo string, pos int) string {
-	newCombo := []rune(combo)
-	num := newCombo[pos] + 1
-	if num > '9' {
-		num = '0'
-	}
-	newCombo[pos] = num
-
-	return string(newCombo)
-}
-
-func decrement(combo string, pos int) string {
-	newCombo := []rune(combo)
-	num := newCombo[pos] - 1
-	if num < '0' {
-		num = '9'
-	}
-	newCombo[pos] = num
-
-	return string(newCombo)
 }
